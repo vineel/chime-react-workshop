2c8d378f3f9a10a0c51afde973714d8998af6f2e
Vineel Shah <vineel@vineel.com>
2023-01-30 17:51:13 -0500

    instant blur works with state variable

--- src/index.js
+++ src/index.js
@@ -32,29 +32,6 @@ async function getMeetingInfo(meetingId) {
   return await response.json()
 }
 
-const joinMeeting = async (meetingManager) => {
-  // Fetch the meeting and attendee data
-  const data = await getMeetingInfo("testMeeting");
-  console.log("Data:", data);
-
-  const meetingSessionConfiguration = new MeetingSessionConfiguration(
-    data.Meeting,
-    data.Attendee
-  );
-  const options = {
-    deviceLabels: DeviceLabels.AudioAndVideo,
-  };
-
-  // Use the join API to create a meeting session
-  await meetingManager.join(meetingSessionConfiguration);
-  console.log("Created meeting session:", data.Meeting, "for:  ", data.Attendee);
-
-  // At this point you can let users setup their devices, or start the session immediately
-  await meetingManager.start();
-  meetingManager.invokeDeviceProvider(DeviceLabels.AudioAndVideo);
-  console.log("Meeting started");
-};
-
 const leaveMeeting = async () => {
   await meetingManager.leave();
 }
@@ -62,11 +39,13 @@ const leaveMeeting = async () => {
 const MyBlurryApp = () => {
   const meetingManager = useMeetingManager();
   const { setIsVideoEnabled, isVideoEnabled, toggleVideo } = useLocalVideo();
-  const { selectedDevice } = useVideoInputs();
+  const { devices, selectedDevice } = useVideoInputs();
   const { isBackgroundBlurSupported, createBackgroundBlurDevice } = useBackgroundBlur();
   const [isVideoTransformCheckBoxOn, setisVideoTransformCheckBoxOn] = useState(false);
 
+  ///////////////////////////////////////////////////////
   useEffect(async function toggleBgBlur() {
+    console.log("useEffect toggleBgBlur");
     let currentDevice = selectedDevice;
     if (isVideoTransformCheckBoxOn) {
       currentDevice = await createBackgroundBlurDevice(selectedDevice);
@@ -75,6 +54,30 @@ const MyBlurryApp = () => {
     }
     await meetingManager.startVideoInputDevice(currentDevice);
   }, [isVideoTransformCheckBoxOn]);
+
+  ///////////////////////////////////////////////////////
+  const joinMeeting = async (meetingManager) => {
+    // Fetch the meeting and attendee data
+    const data = await getMeetingInfo("testMeeting");
+    console.log("Data:", data);
+
+    const meetingSessionConfiguration = new MeetingSessionConfiguration(
+      data.Meeting,
+      data.Attendee
+    );
+    const options = {
+      deviceLabels: DeviceLabels.AudioAndVideo,
+    };
+
+    // Use the join API to create a meeting session
+    await meetingManager.join(meetingSessionConfiguration);
+    console.log("Created meeting session:", data.Meeting, "for:  ", data.Attendee);
+
+    // At this point you can let users setup their devices, or start the session immediately
+    await meetingManager.start();
+    console.log("Meeting started");
+    setisVideoTransformCheckBoxOn(true);
+  };
 
   // join a meeting from the query string
   const queryString = window.location.search;
@@ -87,11 +90,12 @@ const MyBlurryApp = () => {
     setisVideoTransformCheckBoxOn((current) => !current);
   };
 
+
   const appContent = (
     <div>
       {isBackgroundBlurSupported && (
         <div>
-          <button id='join' onClick={() => {joinMeeting(meetingManager)}}>
+          <button id='join' onClick={() => { joinMeeting(meetingManager) }}>
             Join Meeting
           </button>
           <button onClick={onBackgroundBlurClick}>
@@ -100,15 +104,15 @@ const MyBlurryApp = () => {
               : 'Enable Background Blur'}
           </button>
           <button onClick={toggleVideo}>
-          {isVideoEnabled ? 'Stop your video' : 'Start your video'}
-        </button>
+            {isVideoEnabled ? 'Stop your video' : 'Start your video'}
+          </button>
         </div>
       )}
       <hr></hr>
       Preview Video
       <div className="previewVideo">
-      <PreviewVideo />
-      </div> 
+        <PreviewVideo />
+      </div>
       <hr></hr>
       <div className="videoTileGrid">
         VideoTileGrid
